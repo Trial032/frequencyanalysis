1aa3a5d581c6d72cfa78ee6c13b47b5f76cd3a71
pavel <pavel@LAPTOP-343ARR55>
2021-11-11 15:13:08 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FrequencyAnalysis fa = new FrequencyAnalysis();
+            fa.Load("frekvencnianalyza.txt");
+        }
+    }
+
+    public class FrequencyAnalysis
+    {
+        Dictionary<string, double> _frequency = new Dictionary<string, double>();
+        public void Load(string filepath)
+        {
+            if (!File.Exists(filepath))
+            {
+                Console.WriteLine("Soubor neexistuje");
+                Console.ReadKey();
+                return;
+            }
+            string[] file = File.ReadAllLines(filepath);
+            foreach (var item in file)
+            {
+                string[] letter = item.Split(";");
+                _frequency.Add(letter[0], Convert.ToDouble(letter[1]));
+            }
+            
+        }
+
+        public void Analysis(string cipher)
+        {
+
+        }
+    }
+}
